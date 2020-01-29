3c04294e94d4b17fb707a82ee895e8a819caa22a
jingsongs <1343122032@qq.com>
2020-01-29 22:34:48 +0800

    Update MedE2Vec_modules.py

--- MedE2Vec/MedE2Vec_modules.py
+++ MedE2Vec/MedE2Vec_modules.py
@@ -1,5 +1,4 @@
 #encoding:utf8
-#@Author:hadoop
 #@File: MedE2Vec_modules.py
 
 import logging
@@ -91,7 +90,7 @@ def ff(inputs, num_units, scope="positionwise_feedforward"):
     return outputs
 
 class MedE2Vec(object):
-    def __init__(self, n_input, d_model, batch_size, maxseq_len,d_ff,num_blocks,num_heads,dropout_rate,log_eps=1e-20,
+    def __init__(self, n_input, d_model, batch_size, maxseq_len,d_ff,num_blocks,num_heads,dropout_rate,log_eps=1e-8,
                  optimizer=tf.train.AdadeltaOptimizer(learning_rate=0.5), init_scale=0.01):
         self.n_input = n_input
         self.d_model = d_model
@@ -109,9 +108,9 @@ class MedE2Vec(object):
         self.i_vec = tf.placeholder(tf.int32)
         self.j_vec = tf.placeholder(tf.int32)
         self.idx = tf.placeholder(tf.int32, shape=[None, self.maxseq_len])
-        self.vi = tf.placeholder(tf.int32)
-        self.vj = tf.placeholder(tf.int32)
-        self.v = self.encode(self.idx, self.embeddings) * 0.1
+        self.vi = tf.placeholder(tf.int32, shape=[None, 1])
+        self.vj = tf.placeholder(tf.int32, shape=[None, 1])
+        self.v = self.encode(self.idx, self.embeddings)*0.1
         self.emb_cost = self._initialize_entity_cost()
         self.vivlcost = self._initialize_visit_cost()
         self.cost = self.emb_cost + self.vivlcost
@@ -119,6 +118,7 @@ class MedE2Vec(object):
         init = tf.global_variables_initializer()
         self.sess = tf.Session()
         self.sess.run(init)
+
 
     def encode(self, xs,embeddings,training=True):
         with tf.variable_scope("encoder", reuse=tf.AUTO_REUSE):
@@ -137,24 +137,29 @@ class MedE2Vec(object):
 
     def _initialize_entity_cost(self):
         norms = tf.reduce_sum(tf.exp(tf.matmul(self.v, tf.transpose(self.v, [0, 2, 1]))), axis=2)
+        #分母使用全局embedding计算， log_sum 会出现nan值， 原因：优化的时候，exp = tf.exp(tf.reduce_sum(wi_emb * wj_emb, axis=2))
+        # e的幂指函数过大 比如（e的2*10的36次方） 计算出现inf
+        #norms = tf.reduce_sum(tf.exp(tf.matmul(self.embeddings, tf.transpose(self.embeddings, [1, 0]))), axis=1)
         wi_emb = tf.gather(self.v, self.i_vec, axis=1)
         wj_emb = tf.gather(self.v, self.j_vec, axis=1)
         exp = tf.exp(tf.reduce_sum(wi_emb * wj_emb, axis=2))
         norms2 = tf.gather(norms, self.i_vec, axis=1)
-        log_sum = tf.reduce_sum(-tf.log(exp / norms2))
-        return log_sum
-
+        #norms2 = tf.gather_nd(norms, tf.reshape(self.i_vec, (-1, 1)))
+        log_sum = tf.reduce_sum(-tf.log(exp / norms2+ self.log_eps))
+        return  log_sum
 
     def _initialize_visit_cost(self):
-        w_emb_relu = self.v
-        norms = tf.reduce_sum(tf.exp(tf.matmul(w_emb_relu, tf.transpose(w_emb_relu, [0, 2, 1]))), axis=2)
-        wi_emb = tf.gather(w_emb_relu, self.vi, axis=0)
-        wj_emb = tf.gather(w_emb_relu, self.vj, axis=0)
-        exp = tf.exp(tf.reduce_sum(wi_emb * wj_emb, axis=2))
-        norms2 = tf.gather(norms, self.vj, axis=0)
-        log_sum = tf.reduce_sum(-tf.log(exp / norms2))
+        #分母、分子为一个batch的数据
+        # self.v：batch_size*104*512-------batch_size*104
+        batch_v = tf.squeeze(tf.layers.dense(self.v, 1),axis=-1)*0.1
+        norms = tf.reduce_sum(tf.exp(tf.matmul(batch_v, tf.transpose(batch_v, [1, 0]))), axis=1)
+        wi_emb = tf.gather_nd(batch_v, self.vi)
+        wj_emb = tf.gather_nd(batch_v, self.vj)
+        exp = tf.exp(tf.reduce_sum(wi_emb * wj_emb, axis=1))
+        # norms2 = tf.gather(norms, self.vj, axis=0)
+        norms2 = tf.gather_nd(norms, self.vj)
+        log_sum = tf.reduce_sum(-tf.log(exp / norms2+self.log_eps))
         return log_sum
-
 
     def partial_fit(self, x=None, i_vec=None, j_vec=None, vi=None, vj=None):
         cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict=
